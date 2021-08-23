ce80b96d6765399c64bcc9677549dfbaf718adec
Frank Seifferth <frankseifferth@posteo.net>
2021-08-23 22:40:26 +0200

    Wrap errors and warnings at 79 rather than 70 columns

--- timesheet_misc.py
+++ timesheet_misc.py
@@ -4,7 +4,7 @@ import sys
 from textwrap import wrap
 
 def __printwrap(msg: str) -> None:
-    print('\n'.join(wrap(msg)), file=sys.stderr)
+    print('\n'.join(wrap(msg, width=79)), file=sys.stderr)
 
 def parser_warning(line_number: int, warning: str) -> None:
     __printwrap(f'Warning while parsing line {line_number+1}: {warning}')
